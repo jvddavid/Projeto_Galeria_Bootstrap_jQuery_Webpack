41827de59733f51700d6c02b0b71d11f6a484988
João Victor <46910583+jvddavid@users.noreply.github.com>
2021-11-15 12:26:16 -0300

    fix: command build

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ Author: João Victor David de Oliveira (j.victordavid2@gmail.com)
 index.js (c) 2021
 Desc: description
 Created:  2021-11-15T04:43:33.898Z
-Modified: 2021-11-15T04:44:06.296Z
+Modified: 2021-11-15T04:47:28.228Z
 */
-
+// * Arquivo SASS
 import './scss/index.scss'
