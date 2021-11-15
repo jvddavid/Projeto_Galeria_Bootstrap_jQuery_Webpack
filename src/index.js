4314cede5dd9c2a552c723a404140932b41b8373
João Victor <46910583+jvddavid@users.noreply.github.com>
2021-11-15 01:44:48 -0300

    link: link the depends files scss to index.scss

---
+++ src/index.js
@@ -0,0 +1,9 @@
+/*
+Author: João Victor David de Oliveira (j.victordavid2@gmail.com)
+index.js (c) 2021
+Desc: description
+Created:  2021-11-15T04:43:33.898Z
+Modified: 2021-11-15T04:44:06.296Z
+*/
+
+import './scss/index.scss'
